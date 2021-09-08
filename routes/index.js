6e067fa41a88bf310d7f7601e4ddee6743c01cfb
Derek <shouldergiant@icloud.com>
2021-09-08 05:44:54 -0400

    styling

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const shouldNotBeLoggedIn = require("../middlewares/shouldNotBeLoggedIn");
+
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", shouldNotBeLoggedIn, (req, res, next) => {
+  res.render("auth/login");
 });
 
-router.get("/profile", (req, res, next) => {
-  console.log(req.query);
-  res.render("profile", { user: req.session && req.session.user });
-});
+// router.get("/profile", (req, res, next) => {
+//   console.log(req.query);
+//   res.render("profile", { user: req.session && req.session.user });
+// });
 
 router.get("/deafAF", (req, res, next) => {
   console.log(req.query);
